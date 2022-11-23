2c66e410ba6ed8722575bc44c6fd2db7b885301c
FERDAUS SK <83603972+Ferdaussk@users.noreply.github.com>
2022-11-23 10:20:32 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+/*Number Counter*/
+$({ Counter: 0 }).animate({
+  Counter: $('.Single').text()
+}, {
+  duration: 1000,
+  easing: 'swing',
+  step: function() {
+    $('.Single').text(Math.ceil(this.Counter));
+  }
+});
